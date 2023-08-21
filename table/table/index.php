da0b036d31cddd3ae4483d39e76ac98de939f6f7
estagiarios-acsu <142044142+estagiarios-acsu@users.noreply.github.com>
2023-08-21 15:56:24 -0300

    Add files via upload

---
+++ table/table/index.php
@@ -0,0 +1,17 @@
+    <div class="search">
+        <input style="margin-top:20px" class="search" type="text" id="searchInput" placeholder="Pesquisar...">
+    </div>
+    <table>
+        <thead>
+            <tr>
+                <th>Instituções</th>
+                <th>Localidade</th>
+                <th>Natureza</th>
+                <th>Zona</th>
+                <th>Endereço</th>
+            </tr>
+        </thead>
+        <tbody id="tableBody">
+            <!-- Os dados da tabela serão carregados aqui -->
+        </tbody>
+    </table>
