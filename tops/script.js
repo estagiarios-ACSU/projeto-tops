1c07299d35ce5e606b73835f7232d802b679d409
estagiarios-acsu <142044142+estagiarios-acsu@users.noreply.github.com>
2023-08-21 15:58:01 -0300

    Add files via upload

---
+++ tops/script.js
@@ -0,0 +1,20 @@
+const searchInput = document.getElementById('searchInput');
+const tableBody = document.getElementById('tableBody');
+
+document.addEventListener('DOMContentLoaded', () => {
+    const searchTerm = searchInput.value;
+    fetch(`../table/data.php?search=${searchTerm}`)
+        .then(response => response.text())
+        .then(data => {
+            tableBody.innerHTML = data;
+        });
+});
+
+searchInput.addEventListener('input', () => {
+    const searchTerm = searchInput.value;
+    fetch(`../table/data.php?search=${searchTerm}`)
+        .then(response => response.text())
+        .then(data => {
+            tableBody.innerHTML = data;
+        });
+});
