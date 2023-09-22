a061bdb5c82f559f8aca0b607b6358395fd13b9f
estagiarios-acsu <142044142+estagiarios-acsu@users.noreply.github.com>
2023-09-22 14:23:47 -0300

    Add files via upload

---
+++ tops/calendarioR/end_date_input.php
@@ -0,0 +1,24 @@
+<?php
+
+include "Conn.php";
+
+$conn = new Conn('tutorial');
+$connect = $conn->connectDb();
+
+if(isset($_POST['event_id'])){
+    $id = $_POST['event_id'];
+    
+    $query = "SELECT end_date FROM events WHERE id = '$id'";
+    $result = $connect->prepare($query);
+    $result->execute();
+
+    $date = null;
+
+    foreach($result as $new){
+        extract($new);
+        $date = $end_date;
+    }
+} 
+
+echo json_encode($date);
+?>
