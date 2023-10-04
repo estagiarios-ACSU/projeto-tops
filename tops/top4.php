a51fa859cc8864069edabcbb3c119d8f3d120d25
estagiarios-acsu <142044142+estagiarios-acsu@users.noreply.github.com>
2023-10-04 14:49:07 -0300

    Update top4.php

--- tops/top4.php
+++ tops/top4.php
@@ -67,7 +67,7 @@ $_SESSION["top"] = 4;
         </div>
 
         <nav class="menu">
-            <ul>
+             <ul>
                 <?php
                     if ($_SESSION["admin"] == "logado") {
                             if (isset($_SESSION["email"])){
@@ -90,6 +90,28 @@ $_SESSION["top"] = 4;
                             }
 
                             echo "<li class='user_logado'>User logado: $email_user</li>";
+                        } else if ($_SESSION["adminMaster"] == "logado"){
+                            if (isset($_SESSION["email"])){
+                                include "../modal/Conn.php";
+                            
+                                $conn = new Conn("top");
+                                $conexao = $conn->connectDb("top");
+
+                                $session_email = $_SESSION['email'];
+                                $query = "SELECT email FROM usuario WHERE email = '$session_email'";
+                                $result = $conexao->query($query);
+                                $result->execute();
+
+                            
+                                $array = $result->fetchall(PDO::FETCH_ASSOC);
+                                foreach($array as $row){
+                                    extract($row);
+                                    $email_user = $email;
+                                    // echo "<script>alert('$email_user')</script>";
+                                }
+
+                                echo "<li class='user_logado'>User logado: $email_user</li>";
+                            }
                         }
 
                     }
@@ -99,7 +121,7 @@ $_SESSION["top"] = 4;
                 <a href="menu/agenda.php"><li>Agenda</li></a>
                 <li class="login">
                 <?php
-                if ($_SESSION["admin"] == "logado") { 
+                if ($_SESSION["admin"] == "logado" || $_SESSION["adminMaster"] == "logado") { 
                     echo"
                     <button id='btn_deslogar' name='btn_deslogar' class='bn632-hover bn18'>Deslogar</button>       
                     <script>
@@ -127,7 +149,6 @@ $_SESSION["top"] = 4;
                         echo"<a href='login.php'><button class='bn632-hover bn18'>Login</button></a>";
                     }
                 ?>
-                </li>
             </ul>
     </header>
 
