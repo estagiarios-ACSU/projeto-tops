a7bab30d37361e1f235ec2ff09ba8d1261b87f1e
estagiarios-acsu <142044142+estagiarios-acsu@users.noreply.github.com>
2023-10-04 14:16:08 -0300

    Update index.php

--- index.php
+++ index.php
@@ -48,6 +48,7 @@ if (isset($_SESSION["admin"])){
             <?php
                 if ($_SESSION["admin"] == "logado") {
                     echo "<div class='cad_admin'><a href='modal/cad_admin.php'><img src='./assets/icons/laura.png' alt='logo-mpe'></a></div>";
+                    echo "<script>document.getElementsByClassName('logo')[0].style.marginLeft = '20px'; </script>";
                 }
             ?>
 
