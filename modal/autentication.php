46cf61f6044bbb9d579e4eaef4d1208afb5673ab
estagiarios-acsu <142044142+estagiarios-acsu@users.noreply.github.com>
2023-10-04 14:21:02 -0300

    Update autentication.php

--- modal/autentication.php
+++ modal/autentication.php
@@ -8,16 +8,22 @@ if(isset($_POST['btn'])){
     $senha = $_POST['password'];
 
     $user = new User($email,$senha,'top');
+    
+    $adminM = $user->adminMaster('usuario');
+    $admin = $user->getLoginAutentication('usuario');
 
-    $user->connectDb('top');
-
-    if($user->getLoginAutentication('usuario')){
+    if($adminM){
+        $_SESSION['adminMaster'] = "Master";
+        $_SESSION['email'] = $email;
+        header("location:../index.php");
+    } else if($admin){
         $_SESSION['admin'] = "admin";
         $_SESSION['email'] = $email;
         header("location:../index.php");
-    }else{
+    }
+    else{
         session_destroy(); 
-        header("location:../index.php");
+        // header("location:../index.php");
         echo"<script>Alert(2)</script>";
         echo "vc n esta logado";
     }
