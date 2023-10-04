d58fcb2c8e1a2f72e0504ce1f1a5ec6907176757
estagiarios-acsu <142044142+estagiarios-acsu@users.noreply.github.com>
2023-10-04 14:22:41 -0300

    Update top2.php

--- tops/top2.php
+++ tops/top2.php
@@ -44,6 +44,7 @@ $_SESSION["top"] = 2;
         <?php
             if ($_SESSION["admin"] == "logado") {
                 echo "<div class='cad_admin'><a href='../modal/cad_admin.php'><img src='../assets/icons/admin.png' alt='logo-mpe'></a></div>";
+                echo "<script>document.getElementsByClassName('logo')[0].style.marginLeft = '20px'; </script>";
             }
         ?>
 
