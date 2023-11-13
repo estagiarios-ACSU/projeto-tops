c629400dfb71d0eb8dc21a9d9439012324f4ed13
estagiarios-acsu <142044142+estagiarios-acsu@users.noreply.github.com>
2023-11-13 20:54:04 -0300

    Add files via upload

--- tops/script.js
+++ tops/script.js
@@ -9,7 +9,7 @@ let column = [['Compromissos','Eixo','Responsável','Observações','Situação'
 const editModal = document.getElementsByClassName('edit-modal');
 const editSaveBtn = document.getElementsByClassName('saveBtn-edit');
 let perfilInput = document.getElementsByClassName('perfilInput');
-let perfilInputEdit = document.getElementsByClassName('perfilInputEdit');
+
 
 // Váriaveis gerais
 
@@ -24,17 +24,18 @@ let pathForm = 'nenhum';
 
 let selectOption = document.getElementsByClassName('columns-infor-choice');
 const tableReference = ['agenda_territorial','perfil_territorial'];
+const tablesNames = ['agenda-','perfil-'];
 
 //Faz com que os dados da tabela sejam atualizados
 async function uploadTable(){
 
-  const searchTerm = searchInput.value;
-
-  const tablesNames = ['agenda-','perfil-'];
   const tablesReferences = ['compromisso','instituicao'];
   //Trás os dados da tabela para o site por ajax
   for(let g = 0;g < 2;g++){
-    let ajaxPedido = await fetch(`../table/data.php?search=${searchTerm}&tableReference=${g}`);
+
+    let indexSelectOption = column[g].indexOf(selectOption[g].value);
+    const searchTerm = searchInput[g].value;
+    let ajaxPedido = await fetch(`../table/data.php?search=${searchTerm}&indexSelectOption=${indexSelectOption}&tableReference=${g}`);
     let ajaxResposta = await ajaxPedido.text();
     
     tableBody[g].innerHTML = ajaxResposta;
@@ -42,6 +43,7 @@ async function uploadTable(){
     //Pega elementos da tabela 
     const row = document.getElementsByClassName(`${tablesNames[g]}tr-perfil`);
     const instituicoes = document.getElementsByClassName(`${tablesNames[g]}instituicao`);
+    let perfilInputEdit = document.getElementsByClassName(`${tablesNames[g]}InputEdit`);
 
     //Adiciona funções nos elementos de editar e deletar da tabela 
     for(let i = 1; i <= row.length;i++){
@@ -68,18 +70,16 @@ async function uploadTable(){
             }).then(response => response.text()).then(data => {
               console.log(data);
             });
-            Swal.fire('Saved!', '', 'success')
+            Swal.fire('Saved!', '', 'success');
           } else if (result.isDenied) {
             Swal.fire('As Mudanças Não Foram Salvas', '', 'info')
           }
-        })
+        }) //acaba aqui o del
 
       })
 
       //Função de editar
       editBtn.addEventListener('click', () => {
-
-        console.log('edit');
 
         //Faz com que o modal apareça
         editModal[g].style.display = 'flex';
@@ -90,9 +90,8 @@ async function uploadTable(){
         let originName = lineContent[0].innerHTML;
 
         for(let c = 0;c < 6;c++){
-          let cInput = g * 5 + c;
           let line1 = document.getElementsByClassName(tablesNames[g]+'line'+i);
-          perfilInputEdit[cInput].value = line1[c].innerHTML;
+          perfilInputEdit[c].value = line1[c].innerHTML;
           instituicaoOrigin = line1[0].innerHTML;
           
         }
@@ -110,14 +109,11 @@ async function uploadTable(){
   return tableBody.innerHTML;
 }
 
-document.addEventListener('click', () => {
-
-  uploadTable();
-})
-
-
 for(let g = 0;g < 2;g++){
   editForm[g].addEventListener('submit', function(event) {
+
+    console.log('edit');
+    let perfilInputEdit = document.getElementsByClassName(`${tablesNames[g]}InputEdit`);
 
     event.preventDefault();
   
@@ -185,7 +181,6 @@ for(let h = 0;h < 2;h++){
 
   saveBtn[h].addEventListener('click', () => {
     pathForm = 'add';
-    console.log("dados salvos com sucesso");
   });
 
   myForm[h].addEventListener('submit', function(event) {
@@ -197,53 +192,16 @@ for(let h = 0;h < 2;h++){
     }
     else{
       if(pathForm == 'add'){
-        if(prompt('deseja add') == 'sim'){
-  
-          const formData = new FormData(this);
-            fetch("../table/add.php", {
-              method: "POST",
-              body: formData
-            }).then(response => response.text())
-            .then(data => {
-                console.log(data); // Exibir a resposta do servidor
-          
-            })
-            .catch(error => {
-                console.error("Erro:", error);
-            });
-          }
-    
-      }
-      
-    }
-    modal[h].style.display = 'none';
-    cancelModal = false;
-    pathForm = 'nenhum';
-
-  })
-
-  for(let h = 0;h < 2;h++){
-    addButton[h].addEventListener('click', () => {
-      modal[h].style.display = 'flex';
-    });
-  
-    saveBtn[h].addEventListener('click', () => {
-      pathForm = 'add';
-      console.log("dados salvos com sucesso");
-    });
-  
-    myForm[h].addEventListener('submit', function(event) {
-  
-    
-      event.preventDefault();
-      if(cancelModal){
-        console.log('fechado');
-      }
-      else{
-        if(pathForm == 'add'){
-          if(prompt('deseja add') == 'sim'){
-    
-            const formData = new FormData(this);
+        const formData = new FormData(this);
+        Swal.fire({
+            title: 'Deseja Realmente Adicionar Esses Dados Na Tabela?',
+            showDenyButton: true,
+            confirmButtonText: 'Sim, adicionar!',
+            denyButtonText: `Não Adicionar`,
+            confirmButtonColor: '#2874fc'
+          }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
               fetch("../table/add.php", {
                 method: "POST",
                 body: formData
@@ -255,103 +213,105 @@ for(let h = 0;h < 2;h++){
               .catch(error => {
                   console.error("Erro:", error);
               });
+              Swal.fire('Adicionado com sucesso!', '', 'success')
+            } else if (result.isDenied) {
+              Swal.fire('As Mudanças Não Foram Salvas','','error')
             }
-      
-        }
-        
+          })
+
+          }
+    
       }
-      modal[h].style.display = 'none';
-      cancelModal = false;
-      pathForm = 'nenhum';
-  
-    
-    })
-    
-  }
+    modal[h].style.display = 'none';
+    cancelModal = false;
+    pathForm = 'nenhum';
 
-  searchInput[h].addEventListener('input', () => {
-    //Pega qual coluna será a base da pesquisa
-    let indexSelectOption = column[h].indexOf(selectOption[h].value);
-    const searchTerm = searchInput[h].value;
-    fetch(`../table/search-data.php?search=${searchTerm}&indexSelectOption=${indexSelectOption}&tableReference=${tableReference[h]}`)
-        .then(response => response.text())
-        .then(data => {
-          const tablesNames = ['agenda-','perfil-'];
-          const tablesReferences = ['compromisso','instituicao'];
-          tableBody[h].innerHTML = data;
-          for(let h = 0;h < 2;h++){
-            addButton[h].addEventListener('click', () => {
-              modal[h].style.display = 'flex';
-            });
-          
-            saveBtn[h].addEventListener('click', () => {
-              pathForm = 'add';
-              console.log("dados salvos com sucesso");
-            });
-          
-            myForm[h].addEventListener('submit', function(event) {
-          
+  })
+
+    //Search Input event is here
+    searchInput[h].addEventListener('input', () => {
+      //Pega qual coluna será a base da pesquisa
+      let indexSelectOption = column[h].indexOf(selectOption[h].value);
+      const searchTerm = searchInput[h].value;
+      fetch(`../table/search-data.php?search=${searchTerm}&indexSelectOption=${indexSelectOption}&tableReference=${tableReference[h]}`)
+          .then(response => response.text())
+          .then(data => {
+            let perfilInputEdit = document.getElementsByClassName(`${tablesNames[h]}InputEdit`);
+            tableBody[h].innerHTML = data;
+
+            const row = document.getElementsByClassName(`${tablesNames[h]}tr-perfil`);
+            const instituicoes = document.getElementsByClassName(`${tablesNames[h]}instituicao`);
+            const tablesReferences = ['compromisso','instituicao'];
+            for(let i = 1;i <= row.length;i++){
+            const delBtn = document.getElementById(`${tablesNames[h]}delBtn${i}`);
+            const editBtn = document.getElementById(`${tablesNames[h]}editBtn${i}`);
+
+            delBtn.addEventListener('click',() => {
+              Swal.fire({
+                title: 'Deseja Realmente Deletar Estes Dados?',
+                showDenyButton: true,
+                confirmButtonText: 'Sim, Deletar',
+                confirmButtonColor:'#2874fc',
+                denyButtonText: `Não Deletar`,
+              }).then((result) => {
+                /* Read more about isConfirmed, isDenied below */
+                if (result.isConfirmed) {
+                  const formData = new FormData();
+                  formData.append(tablesReferences[h],instituicoes[i-1].innerHTML);
             
-              event.preventDefault();
-              if(cancelModal){
-                console.log('fechado');
-              }
-              else{
-                if(pathForm == 'add'){
-                  if(prompt('deseja add') == 'sim'){
-            
-                    const formData = new FormData(this);
-                      fetch("../table/add.php", {
-                        method: "POST",
-                        body: formData
-                      }).then(response => response.text())
-                      .then(data => {
-                          console.log(data); // Exibir a resposta do servidor
-                    
-                      })
-                      .catch(error => {
-                          console.error("Erro:", error);
-                      });
-                    }
-              
+                  const deleteAjax = fetch("../table/del.php", {
+                    method: "POST",
+                    body: formData
+                  }).then(response => response.text()).then(data => {
+                    console.log(data);
+                  });
+                  Swal.fire('Saved!', '', 'success')
+
+                } else if (result.isDenied) {
+                  Swal.fire('As Mudanças Não Foram Salvas', '', 'info')
                 }
+              }) //acaba aqui o del
+
+            })
+
+            editBtn.addEventListener('click', () => {
+
+              console.log('edit');
+      
+              //Faz com que o modal apareça
+              editModal[h].style.display = 'flex';
+      
+      
+              let lineContent = document.getElementsByClassName(tablesNames[h]+'contentLine'+i);
+      
+              let originName = lineContent[0].innerHTML;
+      
+              for(let c = 0;c < 6;c++){
+                let line1 = document.getElementsByClassName(tablesNames[h]+'line'+i);
+                perfilInputEdit[c].value = line1[c].innerHTML;
+                instituicaoOrigin = line1[0].innerHTML;
                 
               }
-              modal[h].style.display = 'none';
-              cancelModal = false;
-              pathForm = 'nenhum';
-          
-              searchInput[h].addEventListener('input', () => {
-                //Pega qual coluna será a base da pesquisa
-                let indexSelectOption = column[h].indexOf(selectOption[h].value);
-                const searchTerm = searchInput[h].value;
-                fetch(`../table/search-data.php?search=${searchTerm}&indexSelectOption=${indexSelectOption}&tableReference=${tableReference[h]}`)
-                    .then(response => response.text())
-                    .then(data => {
-                      const tablesNames = ['agenda-','perfil-'];
-                      const tablesReferences = ['compromisso','instituicao'];
-                      tableBody[h].innerHTML = data;
-                      console.log(data)
-          
-                      const row = document.getElementsByClassName(`${tablesNames[h]}tr-perfil`);
-                      const instituicoes = document.getElementsByClassName(`${tablesNames[h]}instituicao`);
-          
-                      //Adiciona funções nos elementos de editar e deletar da tabela 
-                      for(let i = 1; i <= row.length;i++){
-
-                  
-                      }
-                    });
-                
+      
+              editSaveBtn[h].addEventListener('click', () => {
+                pathForm = 'add';
               })
+      
+            })
+          }
+          })
   
+          })
+    
   }
-  
-  
-}
+
+document.addEventListener('click', () => {
+
+  uploadTable();
+})
+
 
 
 
 let cancelModal = false;
-
 
