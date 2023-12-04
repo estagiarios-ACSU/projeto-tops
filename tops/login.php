1e601814d2e053cf4393c07f9e30306c6529487f
estagiarios-acsu <142044142+estagiarios-acsu@users.noreply.github.com>
2023-12-04 12:29:24 -0300

    Add files via upload

--- tops/login.php
+++ tops/login.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+session_start();
+?>
 
 <head>
     <meta charset="UTF-8">
@@ -7,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="../css/login.css">
+    <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
     <title>Document</title>
 </head>
 
@@ -40,6 +44,15 @@
         <div class="container-right">
             
         </div>
+
+        <?php
+        
+        if(isset($_SESSION['loginError'])){
+            unset($_SESSION['loginError']);
+            echo '<script>Swal.fire("Você não está logado!");</script>';
+        }
+
+        ?>
     </main>
 </body>
 
